8ddd931bfc527600983e0fd6110b1658f100fb9e
Maikel P C Finck <maikel@ciandt.com>
2022-04-10 20:20:54 -0300

    Adding javascript from Bootstrap library.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
 
 import './App.css';
+import './scripts/bootstrap/bootstrap';
 
 function App() {
   return (
---
+++ src/scripts/bootstrap/bootstrap.js
@@ -0,0 +1,20 @@
+// Import just what we need
+
+// import '../../../node_modules/bootstrap/js/dist/alert.js';
+// import '../../../node_modules/bootstrap/js/dist/button.js';
+// import '../../../node_modules/bootstrap/js/dist/carousel.js';
+// import '../../../node_modules/bootstrap/js/dist/collapse.js';
+// import '../../../node_modules/bootstrap/js/dist/dropdown.js';
+// import '../../../node_modules/bootstrap/js/dist/modal.js';
+// import '../../../node_modules/bootstrap/js/dist/offcanvas.js';
+// import '../../../node_modules/bootstrap/js/dist/popover.js';
+// import '../../../node_modules/bootstrap/js/dist/scrollspy.js';
+// import '../../../node_modules/bootstrap/js/dist/tab.js';
+// import '../../../node_modules/bootstrap/js/dist/toast.js';
+// import '../../../node_modules/bootstrap/js/dist/tooltip.js';
+
+// ##################################
+// ## Load all Bootstrap library.  ##
+// ## THIS HAS PERFORMANCE ISSUES. ##
+// ##################################
+// @import "../../../node_modules/bootstrap/js/dist/bootstrap.min.js";
