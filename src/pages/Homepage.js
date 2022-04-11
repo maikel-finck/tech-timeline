33648a51b0b892c23bc91dee9c5dd9ae7d4b9bb3
Maikel P C Finck <maikel@ciandt.com>
2022-04-10 23:38:43 -0300

    Markup structure for Homepage component.

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,7 +5,15 @@ const Homepage = () => {
   return (
     <>
       <Header />
-      <h1 className="title">Tech Timeline: Homepage</h1>
+      <div className="container-fluid content">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1 className="title">Tech Timeline: Homepage</h1>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
