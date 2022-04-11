2de42164b41188cfaa7a44ab4cedcc0844b264d7
Maikel P C Finck <maikel@ciandt.com>
2022-04-10 23:32:09 -0300

    Styling the Not Found component.

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,19 @@
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
+
 const NotFound = () => {
   return (
     <>
-      <h1 className="title">404 - Not found</h1>
+      <Header />
+      <div className="container not-found">
+        <div className="row">
+          <div className="col">
+            <h1 className="title">404 - Not found</h1>
+            <h2 className="sub-title">Something went wrong with the space-time quantum entanglement communication...</h2>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
