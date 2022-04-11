a431699e0e08536484217871ba45f8bb86d39552
Maikel P C Finck <maikel@ciandt.com>
2022-04-10 23:26:03 -0300

    Adding Footer component.

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,18 @@
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="container-fluid footer border-top">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              &copy; <a href="/">maifin.info</a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,12 @@
 import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
 
 const Homepage = () => {
   return (
     <>
       <Header />
       <h1 className="title">Tech Timeline: Homepage</h1>
+      <Footer />
     </>
   );
 }
